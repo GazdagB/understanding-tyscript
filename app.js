c80831b34d9627d84312364c60b5de7de3c250d8
GazdagB <gazdagbal@gmail.com>
2024-12-27 16:21:15 +0100

    Basics 2

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, prefix) {
+    if (prefix === void 0) { prefix = ""; }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(prefix + result);
+    }
+    return result;
 }
 ;
 var number1 = "5";
 var number2 = 2.8;
-var result = add(Number(number1), Number(number2));
+var printResult = true;
+var prefix = "Result: ";
+var result = add(Number(number1), Number(number2), printResult, prefix);
 console.log(result);
