59d6f29df8633bb7617d2496e8be44f0472133db
Gazdag Balázs <gazdagbal@gmail.com>
2024-12-28 10:21:16 +0100

    Configure TS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+"use strict";
 // Good to use when you wont know what it will be
-var userInput;
-var userName;
+let userInput;
+let userName;
 userInput = 5;
 userInput = 'Max';
 //Later if you type check it you can reasign it to other variables.
@@ -11,4 +12,4 @@ if (typeof userInput === 'string') {
 function generateError(message, code) {
     throw { message: message, errorCode: code };
 }
-var result = generateError('An error occured', 500);
+const result = generateError('An error occured', 500);
